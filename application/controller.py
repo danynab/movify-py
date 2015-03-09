4b7c17f15a6cebbe1c5c77023b0f0f5505cabef3
Dani Meana <danielmeanallorian@gmail.com>
2015-03-09 14:15:00 +0100

    Add 'after request' to log IP and User Agent

--- application/controller.py
+++ application/controller.py
@@ -1,7 +1,19 @@
 from application import app
-from flask import redirect
+from flask import redirect, request
 
 __author__ = 'Dani Meana'
+
+
+@app.after_request
+def print_ip(response):
+    try:
+        ip = request.headers.environ['HTTP_X_FORWARDED_FOR']
+    except KeyError:
+        ip = 'NO-IP'
+    user_agent = request.headers.environ['HTTP_USER_AGENT']
+    print()
+    print(('IP: ' + ip if ip else 'None') + ((' - UserAgent: ' + user_agent) if user_agent else ''))
+    return response
 
 
 @app.route("/", methods=["GET"])
