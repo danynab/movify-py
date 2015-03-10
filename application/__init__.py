11c471eabcdd16c20fbb0bca99be33715933f09b
Dani Meana <danielmeanallorian@gmail.com>
2015-03-10 17:02:53 +0100

    Login and logout work

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 __author__ = 'Dani Meana'
 
 prefix = "/movify"
 
 app = Flask(__name__, static_path=prefix + "/static")
+app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/movify.db'
 db = SQLAlchemy(app)
 
--- application/controller.py
+++ application/controller.py
@@ -1,20 +1,56 @@
+from functools import wraps
 from application import app, prefix
-from flask import redirect, request, render_template, url_for
+from flask import redirect, request, render_template, url_for, session, g
 from application.services import user_service, movie_service, rate_service
+import hashlib
 
 __author__ = 'Dani Meana'
 
+SESSION_ID_KEY = "session_id"
+USERNAME_KEY = "username"
 
-@app.after_request
-def print_ip(response):
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if SESSION_ID_KEY in session and USERNAME_KEY in session:
+            session_id = session[SESSION_ID_KEY]
+            username = session[USERNAME_KEY]
+            if session_id == _calculate_session_id():
+                user = user_service.get(username)
+                if user is not None:
+                    return f(*args, **kwargs)
+        return redirect(url_for('show_login'))
+    return decorated_function
+
+
+def _calculate_session_id():
+    ip = g.get("ip", "")
+    user_agent = g.get("user_agent", "")
+    session_id_str = ip + user_agent
+    return _to_md5(_to_md5(session_id_str))
+
+
+def _to_md5(string):
+    return hashlib.md5(string.encode("utf")).hexdigest()
+
+
+@app.before_request
+def print_ip():
     try:
         ip = request.headers.environ['HTTP_X_FORWARDED_FOR']
     except KeyError:
-        ip = 'NO-IP'
+        try:
+            ip = request.headers.environ['REMOTE_ADDR']
+        except KeyError:
+            ip = None
+    if not ip:
+        ip = 'NO IP'
     user_agent = request.headers.environ['HTTP_USER_AGENT']
     print()
     print(('IP: ' + ip if ip else 'None') + ((' - UserAgent: ' + user_agent) if user_agent else ''))
-    return response
+    g.ip = ip
+    g.user_agent = user_agent
 
 
 @app.route(prefix + "/", methods=["GET"])
@@ -44,9 +80,20 @@ def do_login():
     password = request.form["password"]
     user = user_service.login(username, password)
     if user is not None:
-        return 'Login successful'
+        session_id = _calculate_session_id()
+        session[SESSION_ID_KEY] = session_id
+        session[USERNAME_KEY] = username
+        return redirect(url_for("index"))
     else:
-        return 'Username or password is not correct'
+        return redirect(url_for("show_login"))
+
+
+@app.route(prefix + "/logout", methods=["GET"])
+@login_required
+def do_logout():
+    session.pop(SESSION_ID_KEY)
+    session.pop(USERNAME_KEY)
+    return redirect(url_for("index"))
 
 
 @app.route(prefix + "/signup", methods=["GET"])
@@ -68,7 +115,12 @@ def do_register():
         return "Email can not be empty"
     if email == confirm_email:
         user = user_service.register(username, password, email)
-        return "Success. User registered" if user is not None else "Username already registered"
+        if user is not None:
+            session_id = _calculate_session_id()
+            session[SESSION_ID_KEY] = session_id
+            session[USERNAME_KEY] = user.username
+            return redirect(url_for("index"))
+        return "Username already registered"
     else:
         return "Emails not equals"
 
@@ -76,12 +128,14 @@ def do_register():
 # Movies
 
 @app.route(prefix + "/movies", methods=["GET"])
+@login_required
 def show_movies():
     movies = movie_service.get_all()
     return render_template('util.html', movies=movies)
 
 
 @app.route(prefix + "/movies", methods=["POST"])
+@login_required
 def save_movie():
     title = request.form["title"]
     synopsis = request.form["synopsis"]
@@ -97,10 +151,11 @@ def save_movie():
 
 
 @app.route(prefix + "/movies/<int:movie_id>/rates", methods=["POST"])
+@login_required
 def rate_movie(movie_id):
     value = request.form["value"]
     users = user_service.get_all()
-    user = users[users.__len__()-1]
+    user = users[users.__len__() - 1]
     rate = rate_service.rate_movie(movie_id, user.username, value)
     return redirect(url_for("show_movies"))
 
@@ -108,6 +163,7 @@ def rate_movie(movie_id):
 # UTIL
 
 @app.route(prefix + "/util", methods=["GET"])
+@login_required
 def show_util():
     movies = movie_service.get_all()
     return render_template('util.html', movies=movies)
