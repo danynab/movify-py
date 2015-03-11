7f52bd8a92153f298c1c05114ed8a92c255c9bfe
Dani Meana <danielmeanallorian@gmail.com>
2015-03-11 20:34:57 +0100

    Add CORS.

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.ext.cors import CORS
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
@@ -9,6 +10,7 @@ prefix = "/movify"
 app = Flask(__name__, static_path=prefix + "/static")
 app.secret_key = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/movify.db'
+cors = CORS(app, resources=r'*', allow_headers='Content-Type')
 db = SQLAlchemy(app)
 
 import application.controller
