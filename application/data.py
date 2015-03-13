7767d2e264fa897e0db5e2d772b6c14bd57640fc
Dani Meana <danielmeanallorian@gmail.com>
2015-03-13 02:15:11 +0100

    Updated prices

--- application/data.py
+++ application/data.py
@@ -5,19 +5,19 @@ subscriptions = [
         "name": "1 month",
         "description": "All movies you love for one month more. You in?",
         "months": 1,
-        "price": 24.99
+        "price": 7.99
     },
     {
         "name": "3 months",
         "description": "Not enough? Add 3 months to your subscription",
         "months": 3,
-        "price": 69.97
+        "price": 19.97
     },
     {
         "name": "1 year",
         "description": "This is just for truly movie lovers. Are you?",
         "months": 12,
-        "price": 260.89
+        "price": 74.89
     }
 ]
 
